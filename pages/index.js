97acc6a40eb2d49b075cd6beb576ff42be90239a
Henrique Guimaraes <henrique.guimaraes@dommus.com.br>
2021-02-28 19:32:05 -0300

    Primeira página

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+export default function Home() {
+    return (
+        <div>
+            <div>Ritápolis</div>
+            <Contador />
+        </div>
+    )
+}
+
+function Contador() {
+    const [contador, setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+}
